8cf7ce5dbef2e107193a0545b20aeaccfcc56d00
Tiago Figueiredo <tiago0712.figueiredo@gmail.com>
2026-04-17 16:27:07 +0100

    Falta o modelo dos users

--- app/data/models/User.js
+++ app/data/models/User.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    //TODO
+    id: { type: Number, required: true, unique: true },
+    nome: { type: String, required: true },
+    apelido: { type: String, required: true },
+    data_criacao: { type: Date, required: true },
+    role: { type: String, enum: ['admin', 'produtor', 'user'], required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true }
 });
 
 const User = mongoose.model('User', userSchema, 'users');
