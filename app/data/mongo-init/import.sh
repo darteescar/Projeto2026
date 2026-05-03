b8b1d999ab67904f58983d8ffd4a74ba97330f3a
Tiago Figueiredo <tiago0712.figueiredo@gmail.com>
2026-05-03 14:03:58 +0100

    fix: path no scrip de importação

--- app/data/mongo-init/import.sh
+++ app/data/mongo-init/import.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Importa o dataset de comentarios para a base de dados recursosEscolares, coleção comentarios
-mongoimport --host localhost --db recursosEscolares --collection comentarios --type json --file /docker-entrypoint-initdb.d/json/comentarios.json --jsonArray
+mongoimport --host localhost --db recursosEscolares --collection comentarios --type json --file /docker-entrypoint-initdb.d/comentarios.json --jsonArray
 
 # Importa o dataset de users para a base de dados recursosEscolares, coleção users
-mongoimport --host localhost --db recursosEscolares --collection users --type json --file /docker-entrypoint-initdb.d/json/users.json --jsonArray
+mongoimport --host localhost --db recursosEscolares --collection users --type json --file /docker-entrypoint-initdb.d/users.json --jsonArray
 
 echo "Import concluído."
